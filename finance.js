bf313e10d3c0788775cc3f8ea77103df08a594c1
Shaugi <muhammad.shaugi@gmail.com>
2022-10-21 13:19:23 +0700

    update lagi

--- finance.js
+++ finance.js
@@ -1,20 +1,21 @@
 
 // >>== FORM INPUT CONTROL ==<<
 
-function formvalidator(){
+function service2validator(){
     var description2 = document.getElementsByName('description2');
-    var qty2 = document.getElementsByName('description2');
-    var unit_price2 = document.getElementsByName('description2');
-    var amount2 = document.getElementsByName('description2');
+    var qty2 = document.getElementsByName('qty2');
+    var unit_price2 = document.getElementsByName('unit_price2');
+    var amount2 = document.getElementsByName('amount2');
 
-    description2.addEventListener('change', function () {
-        console.log("changed");
-        console.log("add tagg");
+    if(description2.value !=null){
+        console.log("changed not null");
         qty2.class += "mandatory";
         qty2.sateraito_class += "mandatory";
         unit_price2.class += "mandatory";
         unit_price2.sateraito_class += "mandatory";
         amount2.class += "mandatory";
         amount2.sateraito_class += "mandatory";
-    });
+    }else{
+        console.log("changed null");
+    }
 }
