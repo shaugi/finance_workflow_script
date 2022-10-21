af35ef856041c0856524ff305f8e68f310794766
Shaugi <muhammad.shaugi@gmail.com>
2022-10-21 13:52:27 +0700

    update

--- finance.js
+++ finance.js
@@ -2,12 +2,13 @@
 // >>== FORM INPUT CONTROL ==<<
 
 function service2validator(){
-    var description2 = document.getElementsByName('description2');
+    var description2 = SateraitoWF.getFormValue(form, 'description2');
     var qty2 = document.getElementsByName('qty2');
     var unit_price2 = document.getElementsByName('unit_price2');
     var amount2 = document.getElementsByName('amount2');
 
-    if(description2.value !=null){
+
+    if(description2.value != null){
         console.log("changed not null");
         qty2.class += "mandatory";
         qty2.sateraito_class += "mandatory";
